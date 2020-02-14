f288a19a0e8219a40a906d28911e28decdd8e0bd
Selma Marega <selmamarega@gmail.com>
2020-02-14 16:39:49 +0000

    first commit

---
+++ scripts.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    $ ("#button1").click(function (){
+      $(".fadeouttag").hide();
+      
+ 
+    });          
+  
+ $ ("#button2").click(function (){
+   $(".fadeouttag").show();
+      
+ 
+    }) ;          
+  
+  });
